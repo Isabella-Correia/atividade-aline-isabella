0d930ad66d31706915f128ae8574b3340312708e
Isabella-Correia <128486492+Isabella-Correia@users.noreply.github.com>
2023-04-06 10:33:59 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function gow(){
+	let gowimg = document.querySelector("img#imagem");
+	let num = Math.trunc(Math.random()*10);
+	
+	if (num == 0){
+		gowimg.setAttribute("src", "img/gow" + num + ".png");
+	}
+	else if (num == 1){
+		gowimg.setAttribute("src", "img/gow" + num + ".png");
+	}
+	else if (num == 2){
+		gowimg.setAttribute("src", "img/gow" + num + ".png");
+	}
+	else if (num == 3){
+		gowimg.setAttribute("src", "img/gow" + num + ".png");
+	}
+	else if (num == 4){
+		gowimg.setAttribute("src", "img/gow" + num + ".png");
+	}
+	else if (num == 5){
+		gowimg.setAttribute("src", "img/gow" + num + ".png");
+	}
+	else if (num == 6){
+		gowimg.setAttribute("src", "img/gow" + num + ".png");
+	}
+	else if (num == 7){
+		gowimg.setAttribute("src", "img/gow" + num + ".png");
+	}
+	else if (num == 8){
+		gowimg.setAttribute("src", "img/gow" + num + ".png");
+	}
+	else if (num == 9){
+		gowimg.setAttribute("src", "img/gow" + num + ".png");
+	}
+}
